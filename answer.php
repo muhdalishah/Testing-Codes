746577c62b62abfda3ec88a629a6b7adecf1c44b
Muhammad Ali Shah <35764345+muhdalishah@users.noreply.github.com>
2022-06-27 05:50:57 -0700

    Add files via upload

---
+++ answer.php
@@ -0,0 +1,198 @@
+<?php
+session_start();
+if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true ){
+
+    header("location: http://localhost/IPD%20Codes/WeekEnd/WeekEnd/login.php" );
+    exit;
+}
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 - Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+         <!-- Sidebar -->
+         <?php include 'sidebar.php'; ?>
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+            <?php include 'navbar.php'; ?>
+                <!-- End of Topbar -->
+
+                
+                <!-- Begin Page Content -->
+                <div class="container-fluid">                
+
+<!--  notfication for both students and teachers  -->
+<?php 
+    include 'db_connection.php';
+    $con = getconnect();
+
+    $email = $_SESSION['email'];
+    $query1 = "select * from users where users.email ='$email'  ";
+    $result = mysqli_query($con , $query1);
+    $rows = mysqli_num_rows($result);
+
+    if($rows>0){
+        while($data = mysqli_fetch_array($result)){
+        $userid =     $data['id'];
+    
+        $query2  = "SELECT * FROM `questions` where userid ='$userid'";
+        $result2 = mysqli_query($con , $query2);
+        $rows2 = mysqli_num_rows($result2);
+        if($rows2>0){
+            while($data2 = mysqli_fetch_array($result2)){
+              $qid =   $data2['qid'];
+             
+
+
+              $query3  = "SELECT * FROM `answers` where qid ='$qid'";
+              $result3 = mysqli_query($con , $query3);
+              $rows3 = mysqli_num_rows($result3);
+              if($rows3>0){
+                  while($data3 = mysqli_fetch_array($result3)){
+                    ?>
+
+    
+<div class="card mt-4">
+  <div class="card-body">
+    <h3>Q: <?php echo $data2['qtitle'] ?> </h3>
+    <p class="card-text">Ans: <?php echo $data3['answer'] ?>  </p>
+    <!-- <a href="#" class="btn btn-primary">Published Date</a>
+    <a href="#" class="btn btn-primary">2022-06-20</a> -->
+  </div>
+</div>
+
+
+<?php
+
+                  }
+                
+                }
+              }    
+    }
+            }}
+    
+    
+    ?>                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                </div>
+            <!-- Footer -->
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy;  IPD Test Website</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-primary" href="logout.php">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="vendor/chart.js/Chart.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="js/demo/chart-area-demo.js"></script>
+    <script src="js/demo/chart-pie-demo.js"></script>
+
+</body>
+
+</html>
